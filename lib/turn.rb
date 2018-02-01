688250e03fce06b94f5b34c72b8be46556916231
Kevin McPherson <kjmcpherson@gmail.com>
2018-02-01 18:02:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,6 +15,10 @@ def valid_move?(board, index)
   index.between?(0, 8) && !position_taken?(board, index)
 end
 
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " " || board[index] == "")
+end
+
 def turn(board)
   puts "Please enter 1-9:"
   i = gets.strip
